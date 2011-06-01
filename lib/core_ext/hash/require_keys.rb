64e797c116c473f15d52978a2d16832f0f7d0c23
Brian Landau <brianjlandau@gmail.com>
2011-06-01 13:43:53 -0400

    Allow keys in hash that aren't required.

--- lib/core_ext/hash/require_keys.rb
+++ lib/core_ext/hash/require_keys.rb
@@ -1,8 +1,5 @@
 class Hash
   def require_keys(*valid_keys)
-    unknown_keys = keys - [valid_keys].flatten
-    raise(ArgumentError, "Unknown key(s): #{unknown_keys.join(", ")}") unless unknown_keys.empty?
-    
     missing_keys = [valid_keys].flatten - keys
     raise(ArgumentError, "Missing some required keys: #{missing_keys.join(", ")}") unless missing_keys.empty?
   end
